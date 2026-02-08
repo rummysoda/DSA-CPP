97141c10b9547065fd74c87a15e22bbfca089daf
Rummy <61748455+rummysoda@users.noreply.github.com>
2026-02-08 17:35:14 +0000

    Update search a 2d matrix.cpp

--- neetcode/Binary Search/search a 2d matrix.cpp
+++ neetcode/Binary Search/search a 2d matrix.cpp
@@ -24,3 +24,42 @@ public:
         return false;
     }
 };
+
+// this one is M log(N) ill try log(NM)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rm = 0;
+        int lm = matrix.size() - 1;
+        int row = 0;
+        while (rm <= lm) {
+            row = (lm+rm) * 0.5;
+            int big = matrix[row].size() - 1;
+            if (target > matrix[row][big]) {
+                rm = row + 1;
+            }
+            else if (target < matrix[row][0]) {
+                lm = row -1;
+            }
+            else {
+                break;
+            }
+        }
+        int l = 0;
+        int r = matrix[row].size() - 1;
+        while ( l <= r) {
+            int med = (l+r)*0.5;
+            if (target == matrix[row][med]) {return true;}
+            if (target > matrix[row][med] ) {
+                l = med +1;
+            }
+            else {
+                r = med -1;
+            }
+        }
+    
+        return false;
+    }
+};
